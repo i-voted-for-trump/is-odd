dcc7eeff5be8f8190e2a7e63b76eae2992ad854f
DYM <dym.sohin@gmail.com>
2017-05-26 18:12:26 +0200

    non-integers are not a valid input

--- index.js
+++ index.js
@@ -8,10 +8,14 @@
 'use strict';
 
 var isNumber = require('is-number');
+var isInteger = require('is-integer');
 
 module.exports = function isOdd(i) {
   if (!isNumber(i)) {
     throw new TypeError('is-odd expects a number.');
+  }
+  if (Number(i) !== Math.floor(i)) {
+    throw new RangeError('is-odd expects an integer.');
   }
   return !!(~~i & 1);
 };
